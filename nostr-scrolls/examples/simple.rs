eaa538c413bab981e97eb08143e167cd1e5530a5
Awiteb <a@4rs.nl>
2026-04-06 22:38:31 +0000

    examples: rewrite simple example to be simpler
    Signed-off-by: Awiteb <a@4rs.nl>

--- nostr-scrolls/examples/simple.rs
+++ nostr-scrolls/examples/simple.rs
@@ -1,26 +1,45 @@
+//! Display all Kind 1 (text note) posts tagged with "#asknostr" authored by the
+//! current user (the one running this program).
+//!
+//! Program parameters:
+//! ```
+//! [
+//!   ["param", "me", "", "public_key", "required"],
+//!   ["param", "limit", "limit the notes number", "number", ""]
+//!   ["param", "relay", "The relay to get the notes from", "relay", ""]
+//! ]
+//! ```
+
 #![no_std]
 #![no_main]
 
 use heapless::format;
-use nostr_scrolls::Filter;
+use nostr_scrolls::{Filter, PublicKey};
 
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    core::arch::wasm32::unreachable()
-}
-
+#[allow(unused_must_use)]
 #[nostr_scrolls::main]
-fn run() {
+fn run(me: PublicKey, mut limit: Option<i32>, relay: Option<&str>) {
+    nostr_scrolls::log("Running simple example: Fetching your notes tagged with #asknostr");
+
     let mut filter = Filter::new();
-    filter.tag('t', "asknostr").unwrap();
-    filter.send_to("wss://relay.damus.io").unwrap();
+    filter.author(&me);
+    filter.kind(1);
+    filter.close_on_eose();
+    filter.tag('t', "asknostr");
+
+    limit = limit.map(|l| l + 1);
+    if let Some(limit) = limit {
+        filter.limit(limit as usize);
+    }
+
+    if let Some(relay) = relay {
+        filter.send_to(relay);
+    }
 
     let sub = filter.subscribe();
     sub.on_event(|event, _| {
-        if event.kind() == 1 {
-            nostr_scrolls::log(&format!(100; "Found {}", event.id_hex()).unwrap()).unwrap();
-            nostr_scrolls::display(&event);
-        }
-        true // close on first event
+        nostr_scrolls::log(&format!(100; "Found {}", event.id_hex()).unwrap()).unwrap();
+        nostr_scrolls::display(&event);
+        false
     });
 }
